81453a84c48f92dfcc8a20be5064d375116fc319
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-04-11 22:00:13 +0200

    Initial commit with basic TypeScript todo setup

---
+++ index.ts
@@ -0,0 +1,51 @@
+interface Todo {
+    id: number;
+    title: string;
+    done: boolean;
+  };
+
+const toDoList: Todo[] = [];
+
+const generateId = () : number => {
+    return Date.now() + Math.floor(Math.random() * 1000);
+};
+
+const createToDoList = (title: string) : void => {
+    const newToDoList: Todo = {
+        id: generateId(),
+        title,
+        done: false
+    };
+    toDoList.push(newToDoList);
+};
+
+createToDoList("Learn TypeScript");
+createToDoList("Keep GitHub green 🌱");
+createToDoList("Make tea");
+createToDoList("Clean desk");
+
+console.log(toDoList);
+
+const doneList: Todo[] = [];
+
+const removeATask = (idToRemove: number) : void => {
+
+    toDoList.forEach((toDo) => {
+        if (toDo.id === idToRemove) {
+          toDo.done = true; 
+          doneList.push(toDo);
+        }
+    });
+
+    const updatedList = toDoList.filter((toDo) => toDo.id !== idToRemove);
+    toDoList.length = 0;  
+    toDoList.push(...updatedList);
+};
+
+console.log("Before:", toDoList);
+
+removeATask(toDoList[3].id);
+
+console.log("After:", toDoList);
+
+console.log("Done tasks:", doneList);
